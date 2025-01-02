3d3f7daeb4be625c3e89c237e6399cc6c34d159f
tehuanmelo <tehuanmelo@gmail.com>
2025-01-02 16:28:18 +0400

    feat: Import and scale background to fit window

--- main.py
+++ main.py
@@ -2,22 +2,29 @@ import pygame
 import random
 import time
 
+from os.path import join
+
 WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Dodge")
+BG_ORIGIN = pygame.image.load(join("assets/images", "bg.jpeg"))
+BG = pygame.transform.scale(BG_ORIGIN, (WIDTH, HEIGHT))
+
+def draw():
+    WIN.blit(BG, (0,0))  
+    pygame.display.update()
+
 
 def main():
-    
-    
     running = True
     while running:
         for event in pygame.event.get():
             if (event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE)):
                 running = False
                 break
-            
-            
+        draw()
     pygame.quit()
-    
+
+
 if __name__ == "__main__":
     main()
