c7ffb712a8ea23d216dba6021b0c0889523850e9
tehuanmelo <tehuanmelo@gmail.com>
2025-01-03 01:20:47 +0400

    chore: Colision     - colision separeted in one function feat: Add a list with laser sprites

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class Player(pygame.sprite.Sprite):
         super().__init__(groups)
         self.original_image = pygame.image.load(join("assets/images", "rocket.png"))
         self.image = pygame.transform.scale(self.original_image, (self.original_image.get_width() // 4, self.original_image.get_height() // 5))
-        self.rect = self.image.get_frect(midbottom=(WINDOW_WIDTH/2, WINDOW_HEIGHT))
+        self.rect = self.image.get_frect(midbottom=(WINDOW_WIDTH/2, WINDOW_HEIGHT-5))
         self.mask = pygame.mask.from_surface(self.image)
         self.speed = 5
         self.width = self.image.get_width()
@@ -37,17 +37,17 @@ class Player(pygame.sprite.Sprite):
         
 
 class Missile(pygame.sprite.Sprite):
-    def __init__(self, groups, pos):
+    def __init__(self, missile, groups, pos):
         super().__init__(groups)
-        self.original_image = pygame.image.load(join("assets/images", "missile.png"))
+        self.original_image = missile
         self.image = pygame.transform.rotate(self.original_image, 180)  # Rotate the scaled image by 180 degrees
         self.rect = self.image.get_rect(midbottom=pos)
-        self.mask = pygame.mask.from_surface(self.image)
+        # self.mask = pygame.mask.from_surface(self.image)
         self.speed = 5
         
     def update(self):
         self.rect.bottom += self.speed
-        if self.rect.bottom == WINDOW_HEIGHT - 100:
+        if self.rect.top > WINDOW_HEIGHT + 100:
             self.kill()
             
         
@@ -59,77 +59,53 @@ def timer_display(start_time):
     WIN.blit(font_surf, font_rect)
 
 
-
+def colision(player, all_missiles, custom_font, game_music):
+    if pygame.sprite.spritecollide(player, all_missiles, True, pygame.sprite.collide_mask):
+        lost_text = custom_font.render("You lost", True, "white")
+        lost_text_rect = lost_text.get_rect(center=(WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
+        WIN.blit(lost_text, lost_text_rect)
+        pygame.display.update()
+        game_music.stop()
+        pygame.time.delay(4000)
+        return True
+    return False
+    
+       
 def game():
     pygame.init()
     clock = pygame.time.Clock()
     start_time = pygame.time.get_ticks()
     
-    hit = False
-    
+    # imports 
     all_sprites = pygame.sprite.Group()
+    all_missiles = pygame.sprite.Group()
     player = Player(all_sprites)
-    star = Missile(all_sprites, (WINDOW_HEIGHT/2, 0))
+    missiles = [pygame.image.load(join("assets/images/laser", f"{i}.png")) for i in range(1,17)]
+    game_music = pygame.mixer.Sound(join("assets/audio", "mountainclimbing.wav"))
+    game_music.play(loops=-1)
+    custom_font = pygame.font.Font(join("assets/fonts", "chopsic.otf"), 40)
     
-    # game_music = pygame.mixer.Sound(join("assets/audio", "mountainclimbing.wav"))
-    # game_music.play(loops=-1)
-    
-    # # player = pygame.Rect(0, 0, PLAYER_WIDTH, PLAYER_HEIGHT)
-    # # player.midbottom = (WIDTH/2, HEIGHT)
-    # player = pygame.image.load(join("assets/images", "spaceship.png")).convert_alpha()
-    # self.rect = player.get_frect(midbottom=(WIDTH/2, HEIGHT))
-    # player_mask = pygame.mask.from_surface(player)
-    
-    # custom_font = pygame.font.Font(join("assets/fonts", "chopsic.otf"), 40)
-    
-    # star_timer_cooldown = 2000
-    # star_timer_creation = 0
-    
-    # stars = []
+    missile_timer_cooldown = 2000
+    missile_timer_creation = 0
     
     running = True
     while running:
-        pass
-        dt = clock.tick(60) / 1000
         
-        # if star_timer_creation >= star_timer_cooldown:
-        #     for _ in range(3):
-        #         x = random.randint(0, WIDTH - STAR_WIDTH)
-        #         # star = pygame.Rect(x, -STAR_HEIGHT, STAR_WIDTH, STAR_HEIGHT)
-        #         star_surface = pygame.Surface((STAR_WIDTH, STAR_HEIGHT)).convert_alpha()
-        #         star_surface.fill("lightblue")  # Assuming white stars for simplicity
-        #         star_rect = star_surface.get_frect(midbottom=(x, -STAR_HEIGHT))
-        #         star_mask = pygame.mask.from_surface(star_surface)
-        #         stars.append((star, star_rect, star_mask))
-        #     star_timer_creation = 0
-        #     star_timer_cooldown = max(200, star_timer_cooldown - 50)
+        missile_timer_creation += clock.tick(60)
+        
+        if missile_timer_creation >= missile_timer_cooldown:
+            for _ in range(3):
+                x = random.randint(0, WINDOW_WIDTH)
+                Missile(random.choice(missiles), (all_sprites, all_missiles), (x, -10))
+            missile_timer_creation = 0
+            missile_timer_cooldown = max(200, missile_timer_cooldown - 50)
             
         
         for event in pygame.event.get():
             if (event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE)):
                 running = False
                 break
-            
-        
-            
-        # for star in stars[:]:
-        #     star.y += STAR_SPEED
-        #     if star.y > HEIGHT:
-        #         stars.remove(star)
-        #     elif star.y + STAR_WIDTH >= self.rect.y and player_mask.overlap():
-        #         stars.remove(star)
-        #         hit = True
-                
-        # if hit:
-        #     lost_text = custom_font.render("You lost", True, "white")
-        #     lost_text_rect = lost_text.get_rect(center=(WIDTH/2, HEIGHT/2))
-        #     WIN.blit(lost_text, lost_text_rect)
-        #     pygame.display.update()
-        #     game_music.stop()
-        #     pygame.time.delay(4000)
-        #     break
-                
-        # draw(start_time, player, self.rect, custom_font, stars)
+       
         all_sprites.update()
         
         WIN.blit(BG, (0,0))
@@ -137,6 +113,9 @@ def game():
         timer_display(start_time)
         
         all_sprites.draw(WIN)
+        
+        if colision(player, all_missiles, custom_font, game_music):
+            break
         
         pygame.display.update()
         
